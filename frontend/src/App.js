8559b771b7e326cfc2b768ed338feae976148656
NavodCaldera <navodcaldera0112@gmail.com>
2025-05-09 00:22:33 +0530

    unit trust

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import UnitTrustRates from "./components/UnitTrustRates";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/unit-trust-rates" element={<UnitTrustRates />} />
+        <Route 
+          path="/" 
+          element={
+            <div className="text-center p-8">
+              <Link to="/unit-trust-rates" className="text-blue-600 text-4xl font-bold hover:underline">
+                Unit Trust
+              </Link>
+            </div>
+          } 
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
