fad34e2442dda5c26938580348bddd7ca1bc8711
NavodCaldera <navodcaldera0112@gmail.com>
2025-05-10 01:38:18 +0530

    other pages

---
+++ frontend/src/components/Savingaccount.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SavingAccount = () => {
+  return (
+    <div className="max-w-7xl mx-auto p-6">
+            {/* Title */}
+            <div className="text-center mb-6">
+                <h1 className="text-4xl font-bold font-sans text-transparent bg-clip-text bg-gradient-to-t from-[#181E23] via-[#18426c] to-[#10cfc8] leading-[1.3] pb-4">
+                    Saving Accounts
+                </h1>
+            </div>
+    </div>
+  );
+};
+
+export default SavingAccount;
